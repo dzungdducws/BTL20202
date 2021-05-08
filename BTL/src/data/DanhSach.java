a212a98e2645b22597950da11aa87eaa60137556
dzungdducws <83849778+dzungdducws@users.noreply.github.com>
2021-05-08 17:36:33 +0700

    Delete BTL directory

--- BTL/src/data/DanhSach.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-import java.util.ArrayList;
-
-public class DanhSach {
-	public static ArrayList<CanBo> list = new ArrayList<CanBo>();
-	public static ArrayList<CanBo> listProcess = new ArrayList<CanBo>();
-	public static ArrayList<CanBoDeIn> listPrint = new ArrayList<CanBoDeIn>();
-}
